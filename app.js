0f998380fd253cfc08f8e4f9b9e6bdd60f4f3a11
Mindaugas Kapickas <mindaugas@mkp.com>
2021-05-17 15:32:40 +0300

    chuck norris jokes js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+"use strict";
 console.log("practice");
 
 // 9 toje sakoje su js sukurti h4 elementa ir i ji ikelti data ir laika i js
@@ -9,3 +10,21 @@ h4El.textContent = data;
 // console.log(data);
 // console.log(laikas);
 document.body.prepend(h4El);
+
+// * * 17. 'aboutPage' sakoj tvarkingai commitinant sukurti siek tiek html, prideti stiliu
+//  * ir parsiusti random juokeli is chuck noris ar kito api. https://api.chucknorris.io/
+
+const norrisJokeUrl = "https://api.chucknorris.io/jokes/random";
+const generateJoke = async () => {
+  const jokeDiv = document.createElement("div");
+  const navBar = document.querySelector(".nav-bar");
+  const resp = await fetch(norrisJokeUrl);
+  const data = await resp.json();
+  console.log(data);
+  jokeDiv.innerHTML = `
+  <h2>${data.value}</h2>`;
+  jokeDiv.style.textAlign = "center";
+  jokeDiv.style.color = "olive";
+  navBar.insertAdjacentElement("afterend", jokeDiv);
+};
+generateJoke();
