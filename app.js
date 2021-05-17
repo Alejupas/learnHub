7332a7e2e6952faa6a3e45d17c5f9b2c6d4244d6
Mindaugas Kapickas <mindaugas@mkp.com>
2021-05-17 14:22:54 +0300

    pridetas laikas js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log("practice");
+
+// 9 toje sakoje su js sukurti h4 elementa ir i ji ikelti data ir laika i js
+//  * ikelti naujai sukurta elementa su append i index.html dokumenta
+
+const h4El = document.createElement("h4");
+const data = new Date().toDateString() + new Date().toTimeString();
+h4El.textContent = data;
+// console.log(data);
+// console.log(laikas);
+document.body.append(h4El);
